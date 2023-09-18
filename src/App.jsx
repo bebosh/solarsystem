71f5369953528c70e7a075bbb688fce1201821bc
Paolo <pieffelleo@gmail.com>
2023-09-18 10:16:00 +0200

    click enets and camera pan

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { OrbitControls } from "@react-three/drei";
-import MainContainer from "./MainContainer";
-import { Canvas } from "@react-three/fiber";
+//import { OrbitControls } from '@react-three/drei';
+import MainContainer from './MainContainer';
+import { Canvas } from '@react-three/fiber';
 function App() {
   return (
-    <Canvas camera={{ fov: 75, near: 0.1, far: 1000, position: [1, 10, 20] }} shadows>
-        <color attach='background' args={["black"]} />
-       <OrbitControls />
+    <Canvas
+      camera={{ fov: 75, near: 0.1, far: 1000, position: [1, 10, 20] }}
+      shadows
+    >
+      <color attach="background" args={['black']} />
+      {/* <OrbitControls /> */}
       <MainContainer />
     </Canvas>
   );
--- src/scenes/Earth.jsx
+++ src/scenes/Earth.jsx
@@ -1,18 +1,25 @@
 /* eslint-disable react/prop-types */
-import { useTexture } from "@react-three/drei";
-import EarthTextureDay from "../assets/earth_day.jpg";
-import EarthTextureNight from "../assets/earth_night.jpg";
-import EarthNormal from "../assets/earth_normal.jpg";
-import EarthSpec from "../assets/earth_specular.jpg";
-import EarthDisp from "../assets/earth_displacement.jpg";
-import { useFrame } from "@react-three/fiber";
-import { memo, useCallback, useRef } from "react";
-import UFO from "./UFO";
-import * as THREE from "three";
-import Moon from "./Moon";
+import { useTexture } from '@react-three/drei';
+import EarthTextureDay from '../assets/earth_day.jpg';
+import EarthTextureNight from '../assets/earth_night.jpg';
+import EarthNormal from '../assets/earth_normal.jpg';
+import EarthSpec from '../assets/earth_specular.jpg';
+import EarthDisp from '../assets/earth_displacement.jpg';
+// eslint-disable-next-line no-unused-vars
+import { useFrame, useThree } from '@react-three/fiber';
+import { memo, useCallback, useRef, useEffect, useState } from 'react';
+import UFO from './UFO';
+import * as THREE from 'three';
+import Moon from './Moon';
 
 const Earth = memo(({ displacementScale }) => {
-  const [earthTexture, earthEmissiveMap, earthNormalMap, earthSpecularMap, earthDisplacementMap] = useTexture([
+  const [
+    earthTexture,
+    earthEmissiveMap,
+    earthNormalMap,
+    earthSpecularMap,
+    earthDisplacementMap,
+  ] = useTexture([
     EarthTextureDay,
     EarthTextureNight,
     EarthNormal,
@@ -20,9 +27,11 @@ const Earth = memo(({ displacementScale }) => {
     EarthDisp,
   ]);
   const earthRef = useRef();
-  const clockRef = useRef(new THREE.Clock())
+  const clockRef = useRef(new THREE.Clock());
+  const [hovered, setHovered] = useState(false);
+  const [followingEarth, setFollowingEarth] = useState(false);
 
-  const earthGroupPositionRef = useRef(new THREE.Vector3(8, 0, 0));
+  //const earthGroupPositionRef = useRef(new THREE.Vector3(8, 0, 0));
 
   const updateEarthPosition = useCallback(() => {
     const angle = clockRef.current.getElapsedTime() * 0.2;
@@ -32,16 +41,44 @@ const Earth = memo(({ displacementScale }) => {
 
     earthRef.current.position.set(x, 0, z);
     earthRef.current.rotation.y += 0.001;
-    earthGroupPositionRef.current = earthRef.current.position;
   }, []);
 
-  useFrame(() => {
+  const toggleFollow = () => {
+    setFollowingEarth((prevFollowingEarth) => !prevFollowingEarth);
+  };
+
+  useEffect(() => {
+    document.body.style.cursor = hovered ? 'pointer' : 'auto';
+  }, [hovered]);
+
+  useFrame(({ camera }) => {
     updateEarthPosition();
+    const earthPositionRef = earthRef.current.position;
+    const earthTargetPosition = new THREE.Vector3(
+      earthPositionRef.x + 10,
+      earthPositionRef.y + 2,
+      earthPositionRef.z + 5
+    );
+    if (followingEarth){ 
+      camera.lookAt(earthPositionRef)
+      camera.position.copy(earthTargetPosition)
+    }else{
+      const originalCameraPosition = new THREE.Vector3(1, 10, 20)
+      const originalCameraTarget = new THREE.Vector3(0, 0, 0)
+      camera.lookAt(originalCameraTarget)
+      camera.position.copy(originalCameraPosition)
+    }
   });
 
   return (
     <group ref={earthRef}>
-      <mesh castShadow receiveShadow>
+      <mesh
+        castShadow
+        receiveShadow
+        onClick={toggleFollow}
+        onPointerOver={() => setHovered(true)}
+        onPointerOut={() => setHovered(false)}
+      >
         <sphereGeometry args={[1, 32, 32]} />
         <meshPhongMaterial
           map={earthTexture}
@@ -52,7 +89,7 @@ const Earth = memo(({ displacementScale }) => {
           displacementScale={displacementScale}
           emissiveMap={earthEmissiveMap}
           emissive={0xffffff}
-          emissiveIntensity={1}
+          emissiveIntensity={hovered ? 20 : 1}
         />
       </mesh>
       <UFO />
